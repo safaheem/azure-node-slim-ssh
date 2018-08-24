2eb4faf9eecd76d875cf1ea1816e7daec36184be
Edison Garcia Chinas <edisga@microsoft.com>
2018-08-24 16:01:58 -0500

    updating

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 set -e
 
 echo "Starting Script ..."
-echo "nameserver 8.8.8.8" > /etc/resolv.conf && echo "nameserver 8.8.8.9" >> /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf && echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 node /code/server.js
