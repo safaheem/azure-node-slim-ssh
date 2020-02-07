edaa91958a4b404721589e768d8cc1078178d57e
Edison Garcia <egachi@live.com>
2020-02-07 10:22:38 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ set -e
 # Get environment variables to show up in SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
+printenv EDGE_PORTAL__CONNECT_URL
 service ssh start
 node /code/server.js
