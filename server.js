cc3529594625cdc01a8ec36a3594c993015f9c5c
Edison Garcia <egachi@live.com>
2020-02-07 10:21:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 // Constants
 var DEFAULT_PORT = 3000;
 var PORT = process.env.PORT || DEFAULT_PORT;
+var TEMP = process.env.EDGE_PORTAL__CONNECT_URL;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World from NodeJS docker \n');
+  res.send('Hello World from NodeJS docker, printing variable: ' + TEMP + '\n');
 });
 
 app.listen(PORT)
